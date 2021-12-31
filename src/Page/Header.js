fd358f2efad954edd44e21f0bf1973031e4edd11
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-31 09:31:19 +0700

    Pengumpulan Tugas 2 ReactJS - NIOMIC

---
+++ src/Page/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>
+        <center>
+          <h4>Home | Product | Kontak | Tentang Kami</h4>
+        </center>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
